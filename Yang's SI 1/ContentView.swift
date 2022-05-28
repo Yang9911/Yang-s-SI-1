82c60079cc99684aa6fdf4c4e94e42eca8e02641
Chia-Hao Yang <yang9911@cres.tc.edu.tw>
2022-05-28 23:11:07 +0800

    Initial Commit

---
+++ Yang's SI 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yang's SI 1
+//
+//  Created by Chia-Hao Yang on 2022/5/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yang's SI 1/Yang_s_SI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Yang_s_SI_1App.swift
+//  Yang's SI 1
+//
+//  Created by Chia-Hao Yang on 2022/5/28.
+//
+
+import SwiftUI
+
+@main
+struct Yang_s_SI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
